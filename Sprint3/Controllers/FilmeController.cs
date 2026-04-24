7eba239fb359a976bcad68be19a8dda9cfe7cc68
LeonamAS <leonam.sales@ba.estudante.senai.br>
2026-04-24 09:42:07 -0300

    Aula 03 - Consultando e Paginando

--- Sprint3/Controllers/FilmeController.cs
+++ Sprint3/Controllers/FilmeController.cs
@@ -8,12 +8,29 @@ namespace Sprint3.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
-    public void AdicionaFilme(Filme filme)
+    public IActionResult AdicionaFilme(Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Genero);
+        return CreatedAtAction(nameof(RecuperaFilmePorId), 
+            new {id = filme.Id},
+            filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
     }
 }
--- Sprint3/Models/Filme.cs
+++ Sprint3/Models/Filme.cs
@@ -4,10 +4,14 @@ namespace Sprint3.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
+
     [Required(ErrorMessage = "O titulo do filme é obrigatório")]
     public string Titulo { get; set; }
+
     [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
     public string Genero { get; set; }
+
     [Required]
     [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
     public int Duracao { get; set;}
